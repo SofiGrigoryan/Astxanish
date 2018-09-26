cba368b8ce053484b24d34c8f5dea28daf32cffd
SofiGrigoryan <sophie.grigoryan.itc@gmail.com>
2018-09-26 14:15:04 +0400

    commited

---
+++ Astxanish.java
@@ -0,0 +1,14 @@
+public class H {
+public static void main(String args[]) {
+int a=9;
+for (int i=1; i<=a; i++) {
+	for (int d=i; d<=a; d++) {
+		System.out.print(" ");
+}
+			for (int j=1; j<=i; j++) {
+		System.out.print("* ");
+}
+	System.out.println();
+}
+}
+}
