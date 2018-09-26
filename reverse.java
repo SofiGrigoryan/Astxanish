c7c424f1e782223622b601e46cb0289d8de018c8
SofiGrigoryan <sophie.grigoryan.itc@gmail.com>
2018-09-27 00:43:05 +0400

    reverse

---
+++ reverse.java
@@ -0,0 +1,11 @@
+public class reverse {
+public static void main(String args[]) {
+int a=5896;
+for (int i=1; i<=4; i++) {
+int b=a%10;
+a=a/10;
+System.out.print(b);
+}
+System.out.println();
+}
+}
